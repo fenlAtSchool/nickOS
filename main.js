e329046cddb5eff7fef078e333f64adaab8c529c
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-06-01 09:01:39 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-jop = [0,0,0,0,0,0]
-
 /*
 Movement +/- x
 Movement +/- z
@@ -11,6 +9,34 @@ Main Action
 Alt Action
 
 (NickOS runs in the +Z direction)
+*/
+
+/* Example program
+
+Stick Programming Lang
+16 Registers, Unlimited memory, System Stack
+X/Y/N/NN/NNN syntax from CHIP-8
+
+REQ - Give 32 more 32-bit addresses
+STM - Push string Y to memory at index X (Will be array of ASCII ints)
+STO - Push register X to memory at index Y
+INR - Store 
+
+
+OP Family:
+P - Plus
+S - Minus
+T - Mult
+D - Div
+M - Mod
+A - And
+N - Not
+O - Or
+X - Xor
+
+CAL - Push PC to Stack 2, JMP to POP
+
+
 */
 user = -1
 
@@ -37,25 +63,35 @@ function ctrack(){
         }
 }
 function jp(){
-        ram[7] = ram[2]
-        ram[8] = ram[3]
+        s[7] = s[2]
+        s[8] = s[3]
         let tmp = ctrack()
-        ram[2] = tmp[0]
-        ram[3] = tmp[1]
+        s[2] = tmp[0]
+        s[3] = tmp[1]
         tmp = movtrack()
-        ram[0] = tmp[0]
-        ram[1] = tmp[2]
-        ram[4] = 0
-        ram[5] = 0
+        s[0] = tmp[0]
+        s[1] = tmp[2]
+        s[4] = 0
+        s[5] = 0
 }
-function allocMem(name){
-        ramdict[
+function setProcess(name){
+        ram[name] = []
+        active.push(name)
+}
+function requestMemory(name){
+        for(let i = 0; i < 256; i++){
+                ram[name].push(0)
+        }
+}
+function killProcess(name){
+        active.splice(active.indexOf(name),1)
+        delete ram[name] = []
 }
 
 function OSboot(){
-        ram = []
-        ramdict = {""};
-        for(let i = 0; i < 512; i++){
-                ram.append
-        }
+        active = []
+        ram = {}
+
+        setProcess('sys')
+        requestMemory('sys')
 }
