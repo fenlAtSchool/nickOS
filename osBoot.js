6fde28cec8a790d925b78a3531b7b2f03a9b9232
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-06-03 21:56:10 -0700

    Update osBoot.js

--- osBoot.js
+++ osBoot.js
@@ -3,7 +3,7 @@ function OSboot(){
         ram = []
         s = [0,0,0,0,0,0,0,0,0]
         display = [] //sulfrox code format used
-        filecount = api.getStandardChes\u{74}\u{49}\u{74}emSlot([-1,0,51],0)["attributes"]["customAttributes"]["pages"][0]
+        filecount = api.getStandardChes\u{74}\u{49}\u{74}emSlot([-1,0,51],0)["attributes"]["customAttributes"]["pages"][0] + 0
         for(let i = 0; i < 64; i++){
                 display.push([])
                 for(let z = 0; z < 64; z++){
