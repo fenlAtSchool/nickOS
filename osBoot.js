6c90d169792c7c3e005c0ab3cf6a724f7149c5b6
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-06-03 21:52:16 -0700

    Create osBoot.js

---
+++ osBoot.js
@@ -0,0 +1,15 @@
+function OSboot(){
+        osOn = true
+        active = []
+        ram = []
+        s = [0,0,0,0,0,0,0,0,0]
+        display = [] //sulfrox code format used
+        filecount = api.getStandardChes\u{74}\u{49}\u{74}emSlot([-1,0,51],0)["attributes"]["customAttributes"]["pages"][0]
+        for(let i = 0; i < 64; i++){
+                display.push([])
+                for(let z = 0; z < 64; z++){
+                        display[display.length - 1].push([97,97])
+                }
+        }
+        task = ["displayFileNames", 0, 0]
+}
