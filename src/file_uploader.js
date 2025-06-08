eaa2d9a958b72e0251bcb0393729124cf0723714
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-06-08 10:31:08 -0700

    Update file_uploader.js

--- src/file_uploader.js
+++ src/file_uploader.js
@@ -1,3 +1,12 @@
 function writeDataToItem(chestPosition, slotIdx, payload) {
     api.setStandardChestItemSlot(chestPosition, slotIdx, "956", 1, undefined, { customDescription: payload });
 }
+file = ""
+file = file.match(/.{1,470}/g)
+files = api.getStandardChestItemSlot([0,0,50],0)
+filecount = files.attributes.customAttributes.pages.0
+for(let i = 0; i < file.length; i++){
+    writeDataToItem([filecount+1,0,50],i+1,file[i])
+}
+files.attributes.customAttributes.pages.0++
+api.setStandardChestItemSlot([0,0,50],0,files)
