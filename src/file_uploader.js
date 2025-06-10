c17645ca74911b7f6d3c06ac0728bef52ee91aca
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-06-10 12:46:41 -0700

    Update file_uploader.js

--- src/file_uploader.js
+++ src/file_uploader.js
@@ -1,13 +1,26 @@
+
+file_ = "api.log('Hello World');"
+file_name = "Hello World!"
+
+
+
+
+
 function writeDataToItem(chestPosition, slotIdx, payload) {
-    api.setStandardChestItemSlot(chestPosition, slotIdx, "956", 1, undefined, { customDescription: payload });
+	api.setStandardChes\u{74}\u{49}\u{74}emSlot(chestPosition, slotIdx, "956",1,undefined,{customDescription: payload})
 }
-file = ""
-file = file.match(/.{1,470}/g)
-files = api.getStandardChestItemSlot([0,0,50],0)
-filecount = files.attributes.customAttributes.pages.0
-api.setBlock([filecount+1,0,50],"Chest")
+file = file_.match(/.{1,470}/g)
+files = "api.getStandardChest"
+files += "ItemSlot([0,0,51],0)"
+files = eval(files)
+filecount = files.attributes.customAttributes.pages[0]
+api.setBlock([parseInt(filecount),0,51],"Chest")
+
+files.attributes.customAttributes.pages[0] = (parseInt(files.attributes.customAttributes.pages[0]) + 1).toString()
+filecount = parseInt(filecount) + 1
+api.setStandardChes\u{74}\u{49}\u{74}emSlot([filecount,0,51],0,"Book",1,undefined,{"customAttributes": {"pages": [file_name]}})
 for(let i = 0; i < file.length; i++){
-    writeDataToItem([filecount+1,0,50],i+1,file[i])
+    writeDataToItem([filecount,0,51],i+1,file[i])
 }
-files.attributes.customAttributes.pages[0]++
-api.setStandardChestItemSlot([0,0,50],0,files)
+api.setStandardChes\u{74}\u{49}\u{74}emSlot([0,0,51],0,'Book',1,undefined,files.attributes)
+m = `Succesfully uploaded file ${file_name} with payload ${file_}`
