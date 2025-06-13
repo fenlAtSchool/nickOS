99bc057d038703dc5a952f0c2e78c41056e8676d
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-06-13 13:25:38 -0700

    Update plot3d.js

--- programs/plot3d.js
+++ programs/plot3d.js
@@ -1,20 +1,50 @@
-
-function inb = (x,y,z) => (x >= y && x <= z)
+		
+	
+const inb = (x,y,z) => (x >= y && x <= z)
 
 function init(){
   render_dist = 10
   fov = 160
-  const nr = 0.3
-  const a = 2
+  a = 0.5
+  i = 0
+  tick = 37  
+  display = []
+  for(let i = 0; i < 64; i++){
+	display.push([])
+	for(let j = 0; j < 128; j++){
+	  display[display.length - 1].push([0,1724])
+	}
+  }
 }
-
+function vecxmatr(x,y){
+	out = []
+	for(let i = 0; i < y[0].length; i++){
+		sum = 0
+		for(let j = 0; j < y.length; j++){
+			sum += x[j] * y[j][i]
+		}
+		out.push(sum)
+	}
+	return out
+}
+function updateDisplay(){
+	for(let i = 0; i < 64; i++){
+		for(let j = 0; j < 128; j++){
+			if(display[i][j][0] != display[i][j][1]){
+				api.setBlock(j-64,i,50,api.blockIdToBlockName(display[i][j][1]))
+				display[i][j][0] = display[i][j][1]
+			}
+		}
+	}
+}
 function project(coord){
   let f = 1 / Math.tan(fov * Math.PI / 90)
-  let q = render_dist / (render_dist - nr)
-  let x = a*f*coord[0]/coord[2]
-  let y = f*coord[1]/coord[2]
-  let z = coords[2]*q*(1-nr)]
-  return [x/z,y/z]
+  let q = render_dist / (render_dist - 0.3)
+  coord.push(1)
+  ret = vecxmatr(coord,[[a*f,0,0,0],[0,f,0,0],[0,0,q,1],[0,0,0-coord[2]*0.3,0]])
+  ret[0] /= ret[3]
+  ret[1] /= ret[3]
+  return ret
 }
 function drawLine(a, b, color){
   let xd = a[0] - b[0]
@@ -23,23 +53,24 @@ function drawLine(a, b, color){
   for(let i = 0; i < distance; i++){
     let x = Math.floor(b[0] + (i * xd)/distance)
     let y = Math.floor(b[1] + (i * xy)/distance)
-    if(inb(x,0,127) && inb(y,0,63){
-      // display[y][x] = color
-      api.setBlock([x-64,64-y,50],api.blockIdToBlockName(color))
+    if(inb(x,0, 127) && inb(y,0,63)){
+      display[y][x][1] = color
     }
   }
 }
 function scale(d){
   d[0] += 1
-  d[1] += 1
-  d[0] *= 64
+  \u{64}[1] += 1
+  \u{64}[0] *= 64
   d[1] *= 32
   d[1] = 63 - d[1]
   return d
 }
 function draw3dline(a,b,color){
+  api.log(`a: ${a} b: ${b}`)
   let d = scale(project(a))
   let e = scale(project(b))
+  api.log(`d: ${project(a)} e: ${project(b)}`)
   drawLine(d,e,color)
 }
 function draw3dtri(k,color){
@@ -47,29 +78,58 @@ function draw3dtri(k,color){
   draw3dline(k[0],k[2],color)
   draw3dline(k[1],k[2],color)
 }
+function csc(){
+	for(let i = 0; i < 64; i++){
+	 for(let j = 0; j < 128; j++){
+		display[i][j][1] = 1724
+	 }
+   }
+}
 
 squareMesh = [
   [[0,0,0],[0,1,0],[1,1,0]],
   [[0,0,0],[1,1,0],[1,0,0]],
-  
   [[1,0,0],[1,1,0],[1,1,1]],
   [[1,0,0],[1,1,1],[1,0,1]],
-  
   [[1,0,1],[1,1,1],[0,1,1]],
   [[1,0,1],[0,1,1],[0,0,1]],
-
-  [[0,0,0],[0,1,0],[1,1,0]],
-  [[0,0,0],[1,1,0],[1,0,0]],
-  
   [[0,0,1],[0,1,1],[0,1,0]],
   [[0,0,1],[0,1,0],[0,0,0]],
-  
+  [[0,1,0],[0,1,1],[1,1,1]],
+  [[0,1,0],[1,1,1],[1,1,0]],
   [[1,0,1],[0,0,1],[0,0,0]],
-  [[1,0,1],[0,0,0],[1,0,0]],
+  [[1,0,1],[0,0,0],[1,0,0]]
 ]
+init()
+function t(){
+  i++
+  i %= squareMesh.length
+  if(i == 0){updateDisplay();csc();tick++;}
+  j = squareMesh[i].map(v => [...v]); s = Math.sin(tick); c = Math.cos(tick)
+  hs = Math.sin(tick/2); hc = Math.cos(tick/2)
+  matrotz = [
+[c,s,0],
+[-s,c,0],
+[0,0,1]
+]
+  matrotx = [
+[0,hc,hs],
+[0,-hs,c],
+[1,0,0]
+]
+  for(let i = 0; i < 3; i++){
+    j[i] = vecxmatr(j[i],matrotx)
+	j[i] = vecxmatr(j[i],matrotz)
+	j[i][2] += 4
+	j[i][0] *= 3
+	j[i][1] *= 3
+  }
+  draw3dtri(j,86)
 
+}
+on = false
 function tick(){
-  for(let i of squareMesh){
-    draw3dtri(i,86)
+  if(on){
+   t()
   }
 }
