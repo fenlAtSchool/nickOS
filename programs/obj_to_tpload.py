48767d4674e5adb54a29e9ef519f75f63d1ed0df
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-06-16 09:58:48 -0700

    Update obj_to_tpload.py

--- programs/obj_to_tpload.py
+++ programs/obj_to_tpload.py
@@ -11,29 +11,20 @@ for x in obj:
     tmp = x.split()
     tmp.append("t")
     if(tmp[0] == "v"):
+        for i in range(1,4):
+            tmp[i] = int(float(tmp[i]) * 100)/100
         vertexes.append([float(tmp[1]),float(tmp[2]),float(tmp[3])])
     if(tmp[0] == "f"):
+        for i in range(1,4):
+            tmp[i] = int(tmp[i]) - 1
         sqm.append([vertexes[tmp[1]],vertexes[tmp[2]],vertexes[tmp[3]]])
 obj.close()
-sqm = [sqm[i:i + 150] for i in range(0, len(sqm), 150)]
+sqm = [sqm[i:i + 48] for i in range(0, len(sqm), 48)]
     
 k = input("Save as: ")
 k = open(k,"w")
 k.write("squareMesh = [] \n")
 for i in sqm:
-    k.write("j = " + str(i))
-    k.write("fpos = [parseInt(api.getStandardChestItemSlot([0,0,52],0).attributes.customAttributes.pages[0]),0,52]\nfor(let m = 0; m < 48; m++){\nfpos[2]++\napi.setBlock(fpos,'Chest')\napi.setStandardChestItemSlot(fpos,0,'Book',1,undefined,{'customAttributes': {'pages': j[m].map(x=>toString(x))  }})\n}\napi.setStandardChestItemSlot([0,0,51],0,'Book',1,undefined,{'customAttributes': {'pages': fpos[2] }})\n")
+    k.write("j = " + str(i) + "\n")
+    k.write("fpos = [parseInt(api.getChestItemSlot([0,0,52]).attributes.customAttributes.pages[0]),0,52]\nfor(let i = 0; i < 2; i++){\n  for(let m = 0; m < 48; m++){\n      api.setBlock(fpos,'Chest')\n        api.setStandardChestItemSlot(fpos,0,'Book',1,undefined,{'customAttributes': {'pages': j[m].map(x=>toString(x))  }})\n  };fpos[2]++\n}\napi.setStandardChestItemSlot([0,0,51],0,'Book',1,undefined,{'customAttributes': {'pages': fpos[2] }})\n\n")
 k.close()
-
-'''
-fpos = [parseInt(api.getChestItemSlot([0,0,52]).attributes.customAttributes.pages[0]),0,52]
-for(let m = 0; m < 48; m++){
-    fpos[2]++
-    api.setBlock(fpos,"Chest")
-    api.setStandardChestItemSlot(fpos,0,'Book',1,undefined,{'customAttributes': {'pages': j[m].map(x=>x)  }})
-}
-api.setStandardChestItemSlot([0,0,51],0,'Book',1,undefined,{'customAttributes': {'pages': fpos[2] }})
-
-fpos = [parseInt(api.getChestItemSlot([0,0,52]).attributes.customAttributes.pages[0]),0,52]\nfor(let m = 0; m < 48; m++){\nfpos[2]++\napi.setBlock(fpos,"Chest")\napi.setStandardChestItemSlot(fpos,0,'Book',1,undefined,{'customAttributes': {'pages': j[m].map(x=>x)  }})\n}\napi.setStandardChestItemSlot([0,0,51],0,'Book',1,undefined,{'customAttributes': {'pages': fpos[2] }})
-
-'''
