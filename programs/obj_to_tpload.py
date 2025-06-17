3d88efd2087727b19bb2a07f0c2d766011668b0e
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-06-17 09:05:52 -0700

    Update obj_to_tpload.py

--- programs/obj_to_tpload.py
+++ programs/obj_to_tpload.py
@@ -12,19 +12,28 @@ for x in obj:
     tmp.append("t")
     if(tmp[0] == "v"):
         for i in range(1,4):
-            tmp[i] = int(float(tmp[i]) * 100)/100
+            tmp[i] = float(tmp[i])
         vertexes.append([float(tmp[1]),float(tmp[2]),float(tmp[3])])
     if(tmp[0] == "f"):
         for i in range(1,4):
             tmp[i] = int(tmp[i]) - 1
-        sqm.append([vertexes[tmp[1]],vertexes[tmp[2]],vertexes[tmp[3]]])
+        m = [vertexes[tmp[1]],vertexes[tmp[2]],vertexes[tmp[3]]]
+        if not m in sqm:
+            sqm.append(m)
 obj.close()
-sqm = [sqm[i:i + 48] for i in range(0, len(sqm), 48)]
-    
+sqm = [sqm[i:i + 108] for i in range(0, len(sqm), 108)]
+sqm[-1] += [[0,0,0],[0,0,0],[0,0,0]] * (108 - len(sqm[-1]))
 k = input("Save as: ")
 k = open(k,"w")
-k.write("squareMesh = [] \n")
 for i in sqm:
     k.write("j = " + str(i) + "\n")
-    k.write("fpos = [parseInt(api.getChestItemSlot([0,0,52]).attributes.customAttributes.pages[0]),0,52]\nfor(let i = 0; i < 2; i++){\n  for(let m = 0; m < 48; m++){\n      api.setBlock(fpos,'Chest')\n        api.setStandardChestItemSlot(fpos,0,'Book',1,undefined,{'customAttributes': {'pages': j[m].map(x=>toString(x))  }})\n  };fpos[2]++\n}\napi.setStandardChestItemSlot([0,0,51],0,'Book',1,undefined,{'customAttributes': {'pages': fpos[2] }})\n\n")
+    k.write("fpos = [0,0,52+parseInt(api.getStandardChestItemSlot([0,0,52],0).attributes.customAttributes.pages[0])]\n")
+    k.write("for(let i = 0; i < 3; i++){\n")
+    k.write("    for(let m = 0; m < 36; m++){\n")
+    k.write("        api.setBlock(fpos,'Chest')\n")
+    k.write("        api.setStandardChestItemSlot(fpos,m,'Book',1,undefined,{'customAttributes': {'pages': j[m].map(x=>JSON.stringify(x))  }})\n")
+    k.write("    }; fpos[2]++\n")
+    k.write("}; api.setStandardChestItemSlot([0,0,52],0,'Book',1,undefined,{'customAttributes': {'pages': [(fpos[2] - 52).toString()]}})\n")
+    k.write("api.log('SUCCESS')\n\n")
+
 k.close()
