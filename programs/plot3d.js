8fee8908a3ad1869210fb276d4f77d4c9d45290c
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-06-20 08:20:29 -0700

    Update plot3d.js

--- programs/plot3d.js
+++ programs/plot3d.js
@@ -7,19 +7,9 @@ function init(){
 	nr = 0.2
 	time = 37  
 	display = []
-	camera = [0,0,0]
+	camera = [0,0,-15]
+	lookDir = [0,0,1]
 	light = normalize([1, 0, 0])
-}
-function vecxmatr(x,y){
-	let out = []
-	for(let i = 0; i < y[0].length; i++){
-		sum = 0
-		for(let j = 0; j < y.length; j++){
-			sum += x[j] * y[j][i]
-		}
-		out.push(sum)
-	}
-	return out
 }
 function updateDisplay(){
 	for(let i = 0; i < 64; i++){
@@ -31,18 +21,100 @@ function updateDisplay(){
 		}
 	}
 }
+function csc(){
+	for(let i = 0; i < 64; i++){
+	 for(let j = 0; j < 128; j++){
+		display[i][j][1] = 90
+		display[i][j][2] = 9999
+	 }
+   }
+}
+function getColor(norm){
+	colors = [1724,8,47,483,32,97,59,6,31,28,29,136,85,946,947,948,84,949,950,951,147,66,86].reverse()
+	if(inb(norm,0,1)){
+		return colors[Math.floor(norm*22)]
+	} else if (norm > 1){
+		return 86
+	} else {
+		return 1724
+	}
+}
+function vecxmatr(x,y){
+	let out = []
+	for(let i = 0; i < y[0].length; i++){
+		sum = 0
+		for(let j = 0; j < y.length; j++){
+			sum += x[j] * y[i][j]
+		}
+		out.push(sum)
+	}
+	return out
+}
+function dotProduct(x,y){
+	return x[0] * y[0] + x[1] * y[1] + x[2] * y[2]
+}
+function difference(x,y){
+	return [x[0]-y[0], x[1]-y[1], x[2]-y[2]]
+}
+function add(x,y){
+	for(let i = 0; i < 3; i++){
+		x[i] += y[i]
+	} return x
+}
+function normalize(x){
+	let dist = Math.sqrt(x[0] ** 2 + x[1] ** 2 + x[2] ** 2)
+	return x.map(v => v/dist)
+}
+function crossProduct(x,y){
+	return [
+	(x[1] * y[2] - x[2] * y[1]),
+	(x[2] * y[0] - x[0] * y[2]),
+	(x[0] * y[1] - x[1] * y[0])
+	]
+}
 function project(coord){
   let f = 1 / Math.tan(fov * Math.PI / 90)
   let q = render_dist / (render_dist - nr)
   let ret = vecxmatr([...coord,1],[
 [a*f  ,0    ,0    ,0    ],
 [0    ,f    ,0    ,0    ],
-[0    ,0    ,q    ,1    ],
-[0    ,0    ,-nr*q,0    ]
+[0    ,0    ,q    ,-nr*q],
+[0    ,0    ,1    ,0    ]
 ])
   ret[0] /= ret[3]
   ret[1] /= ret[3]
   return ret
+}
+function convert(camera, pos, up){
+	let newF = normalize(difference(pos, camera))
+	let a = dotProduct(up,newF)
+	a = newF.map(v => v*a)
+	let newUp = normalize(difference(up,a))
+	let newRight = crossProduct(newUp,newF)
+	let mat = [
+		[newRight[0],newRight[1],newRight[2],1],
+		[newUp[0],newUp[1],newUp[2],1],
+		[newF[0],newF[1],newF[2],1],
+		[pos[0],pos[1],pos[2],1]
+	]
+
+matrix.m[3][0] = -(m.m[3][0] * matrix.m[0][0] + m.m[3][1] * matrix.m[1][0] + m.m[3][2] * matrix.m[2][0]);
+		matrix.m[3][1] = -(m.m[3][0] * matrix.m[0][1] + m.m[3][1] * matrix.m[1][1] + m.m[3][2] * matrix.m[2][1]);
+		matrix.m[3][2] = -(m.m[3][0] * matrix.m[0][2] + m.m[3][1] * matrix.m[1][2] + m.m[3][2] * matrix.m[2][2]);
+		matrix.m[3][3] = 1.0f;
+}
+function invert(m){
+	let mat = []
+	mat.push([m[0][0],m[1][0],m[2][0],0])
+	mat.push([m[0][1],m[1][1],m[2][1],0])
+	mat.push([m[0][2],m[1][2],m[2][2],0])
+
+	mat.push([
+		-(m[3][0] * mat[0][0] + m[3][1] * mat[1][0] + m[3][2] * mat[2][0]),
+		-(m[3][0] * mat[0][1] + m[3][1] * mat[1][1] + m[3][2] * mat[2][1]),
+		-(m[3][0] * mat[0][2] + m[3][1] * mat[1][2] + m[3][2] * mat[2][2]), 1
+	])
+	return mat
 }
 function drawLine(a, b, color, z){
   let xd = a[0] - b[0]
@@ -58,23 +130,6 @@ function drawLine(a, b, color, z){
 			}
     }
   }
-}
-function dotProduct(x,y){
-	return x[0] * y[0] + x[1] * y[1] + x[2] * y[2]
-}
-function difference(x,y){
-	return [x[0]-y[0], x[1]-y[1], x[2]-y[2]]
-}
-function normalize(x){
-	let dist = Math.sqrt(x[0] ** 2 + x[1] ** 2 + x[2] ** 2)
-	return x.map(v => v/dist)
-}
-function crossProduct(x,y){
-	return [
-	(x[1] * y[2] - x[2] * y[1]),
-	(x[2] * y[0] - x[0] * y[2]),
-	(x[0] * y[1] - x[1] * y[0])
-	]
 }
 function fillTri(n,color, mz){
 	let x = n[0]
@@ -99,25 +154,17 @@ function scale(d){
   return [d[0],d[1]]
 
 }
-function csc(){
-	for(let i = 0; i < 64; i++){
-	 for(let j = 0; j < 128; j++){
-		display[i][j][1] = 90
-		display[i][j][2] = 9999
-	 }
-   }
-}
 function getNormal(tri){
 	let diff = [difference(tri[1],tri[0]),difference(tri[2],tri[0])]
 	let normal = crossProduct(diff[0],diff[1])
 	return normalize(normal)
 }
-function draw3dtri(k,color){
+function draw3dtri(k,matCam){
 	let n = getNormal(k)
 	let tmp = difference(k[0],camera)
 	if(dotProduct(n,tmp) < 0){
-		let tritorend = k.map(v => scale(project(v)))
-		color = getColor(dotProduct(light,n)/4 + 0.5)
+		let tritorend = k.map(v => scale(project(vecxmatr(v,matCam))))
+		let color = getColor(dotProduct(light,n)/4 + 0.5)
 		fillTri(tritorend,color,(k[0][2] + k[1][2] + k[2][2])/3)
 	}
 	/*
@@ -125,33 +172,7 @@ function draw3dtri(k,color){
   	drawLine(k[0],k[2],color+1)
   	drawLine(k[1],k[2],color+1)
 	*/
-	
 }
-function getColor(norm){
-	colors = [1724,8,47,483,32,97,59,6,31,28,29,136,85,946,947,948,84,949,950,951,147,66,86].reverse()
-	if(inb(norm,0,1)){
-		return colors[Math.floor(norm*22)]
-	} else if (norm > 1){
-		return 86
-	} else {
-		return 1724
-	}
-}
-
-squareMesh = [
-  [[0,0,0],[0,1,0],[1,1,0]],
-  [[0,0,0],[1,1,0],[1,0,0]],
-  [[1,0,0],[1,1,0],[1,1,1]],
-  [[1,0,0],[1,1,1],[1,0,1]],
-  [[1,0,1],[1,1,1],[0,1,1]],
-  [[1,0,1],[0,1,1],[0,0,1]],
-  [[0,0,1],[0,1,1],[0,1,0]],
-  [[0,0,1],[0,1,0],[0,0,0]],
-  [[0,1,0],[0,1,1],[1,1,1]],
-  [[0,1,0],[1,1,1],[1,1,0]],
-  [[1,0,1],[0,0,1],[0,0,0]],
-  [[1,0,1],[0,0,0],[1,0,0]]
-]
 init()
 function t(){
  	 j = api.getStandardChestItemSlot([0,0,52+curr_page],curr_tri).attributes.customAttributes.pages
@@ -168,12 +189,15 @@ function t(){
 	[0,-hs,c],
 	[1,0,0]
 	]
+	let up = [0,1,0]
+	let target = add(camera,lookDir)
+	let matCam = invert(convert(camera,target,up))
   	for(let m = 0; m < 3; m++){
 		j[m] = vecxmatr(j[m],matrotx)
 		j[m] = vecxmatr(j[m],matrotz)
 		j[m][2] += 10
 	}
-	draw3dtri(j,40 + curr_page)
+	draw3dtri(j,matCam)
 	
 }	
 on = false
