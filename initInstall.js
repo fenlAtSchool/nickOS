4c2a6e58340c2d0dc09fbe7e7ece49f08ae72ddd
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-07-06 18:19:13 -0700

    Update and rename src/resetAllFiles.js to initInstall.js

---
+++ initInstall.js
@@ -0,0 +1,3 @@
+api.setBlock(0,0,51,"Chest")
+api.setStandardChestItemSlot([0,0,51],"Net",1,undefined, {customDescription: "~"} )
+api.setStandardChestItemSlot([0,0,51],"Net",1,undefined, {customDescription: ".fol"} )
--- src/resetAllFiles.js
+++
@@ -1,5 +0,0 @@
-j = "api.setStandardChest"
-
-j += "ItemSlot([0,0,51],0,'Book',1,undefined,{'customAttributes': {'pages':['0']}})"
-api.log(j)
-eval(j)
