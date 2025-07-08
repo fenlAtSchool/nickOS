22d634e6222b160b737f78fbe24407581cc238a2
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-07-07 17:35:00 -0700

    Update file_uploader.js

--- src/file_uploader.js
+++ src/file_uploader.js
@@ -2,7 +2,7 @@ contents = "NickOS File Uploader Shell"
 fileName = "demo"
 extension = ".txt"
 
-contents = contents.match(/.{1,256}/g)
+contents = contents.match(/.{1,470}/g)
 while(contents.length < 34){
 	contents.push("")
 }
