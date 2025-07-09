593b9e9bef4280363312e97ff31c80f80634d774
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-07-09 12:04:18 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -490,7 +490,7 @@ function tick(){
 				task = ["waitTC",["folderMenuClicked"]]
 				break
 			case "about":
-				dtxt(0,0,"NickOS V2.5.2 (c) 2025 fenl_")
+				dtxt(0,0,"NickOS V2.6.3 (c) 2025 fenl_")
 				dtxt(0,6,"GPLV3 github.com/tendergalaxy/nickos")
 				dtxt(0,18,"Credits to the_ccccc, sulfrox, delfineon, and nickname")
 				dtxt(0,30,"Click to exit")
