bea8d9c3675b827fbf197f427ddcbdd07782f914
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-07-28 08:58:31 -0700

    Update initInstall.js

--- src/initInstall.js
+++ src/initInstall.js
@@ -1,3 +1,3 @@
 api.setBlock(0,0,51,"Chest")
-api.setStandardChestItemSlot([0,0,51],0,"Net",1,{customDescription: "~"} )
-api.setStandardChestItemSlot([0,0,51],1,"Net",1,{customDescription: ".fol"} )
+api.setStandardChestItemSlot([0,0,51],0,"Net",1,undefined,{customDescription: "~"} )
+api.setStandardChestItemSlot([0,0,51],1,"Net",1,undefined,{customDescription: ".fol"} )
