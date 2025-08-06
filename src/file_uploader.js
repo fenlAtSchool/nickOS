640d1067e9ff7949a7766bf2e5dce7835d985b5b
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-08-06 09:57:17 -0700

    Update file_uploader.js

--- src/file_uploader.js
+++ src/file_uploader.js
@@ -6,7 +6,7 @@ if(contents == ""){
 	contents = []
 }
 if(typeof(contents) == "string"){
-	contents = contents.match(/.{1,470}/g)
+	contents = contents.match(/.{1,460}/g)
 }
 while(contents.length % 36 != 0){
 	contents.push("")
