994270461b267ab6f1922464f7dc21ebe07b9db4
fenlAtSchool <alechan32@nuevaschool.org>
2025-09-11 12:13:00 -0700

    Update module-minos-core.js

--- experimental/module-minos-core.js
+++ experimental/module-minos-core.js
@@ -1,14 +1,67 @@
-function drawDisplay(id){
-  let f = Array(config.displayx * config.displayy).fill({str: '\u2588', style: {color: [255,255,255]}})
-  for(let i of windows){
-    for(let j = 0; j < i.x; j++){
-      for(let z = 0; z < i.y){
-        f[display.x * (i.sy + j) + i.sx + z].style.color = win[i.y][i.x]
-      }
+
+function getFile(x){
+  try{
+    return getBlockData(1e5,followPath(x),0).persisted.shared
+  } catch {
+    throw new Error("FileNotFoundError: ", x)
+    return false
+  }
+}
+function setFile(x,z){
+  api.setBlockData(1e5,followPath(x),0,z)
+}
+function setFileAttribute(x,a,z){
+  let m = getFile(x)
+  m[a] = z
+  setFile(x,m)
+}
+function followPath(x, f=0){
+  if(typeof(x) == "number"){
+    return x
+  }
+  if(typeof(x) == "string"){
+    x = x.split("/")
+  }
+  for(let i of x){
+    f = (getFile(f).contents).map(m => getFile(m))
+    f = f.map(m => m.name + m.extension).indexOf(i)
+    if(f === -1){
+      throw new Error("InvalidFilePathError: ", x)
+      return false
     }
   }
-  api.setFlyingMiddleMessage(id,f,0)
+  return f
 }
+function deleteFile(z,x){
+  z = followPath(z)
+  x = followPath(x)
+  let m = getFile(x)
+  if(m.extension == ''){
+    m.contents.forEach(i => deleteFile(x, i) )
+  }
+  setFile(x, {})
+  let f = getFile(z)
+  f.contents.splice(f.indexOf(x), 1)
+  setFile(z, f)
+}
+function newFile(z,x){
+  z = followPath(z)
+  let f = getFile(z)
+  let r = getFile(-1)
+  r.fileCount++
+  f.contents[f.contents.length] = r.fileCount
+  setFile(z, f)
+  setFile(r, x)
+  setFile(-1, r)
+  log("minfs", `Succesful File Created: ${x}`)
+  return r.fileCount
+}
+
+
+
+
+
+
 function boot(){
   functions = {toRun: [], results: {}}
   requestExecFunction('init()', bootupCode)
@@ -29,33 +82,6 @@ function executeCFF(extension, data){
   if(tr != "HALT"){
     requestExecFunction(`executeCFF(extension, data)`)
     return tr
-  }
-}
-
-function dtxt(win, x,y,f, xlim=win.length, ylim=win[0].length){
-  dx = x
-  for(let i = 0; i < f.length && y < ylim; i++)[
-    if(f[i] === '\n'){
-      y += 4
-      continue
-    }
-    font[f[i]].forEach((j,fy) => fy.forEach((m,fx) => win[y+fy][x+fx]=Array(3).fill(255*(m!="#")) ))
-    x += 4
-    if(x > xlim - 4){
-      x = dx
-    }
-  }
-}
-
-function drawBoxOutline(x1,x2,y1,y2){
-  let m = [0,0,0]
-  for(let i = y1; i < y2; i++){
-    display[i][x1] = m
-    display[i][x2] = m
-  }
-  for(let i = x1; i < x2; i++){
-    display[y1][i] = m
-    display[y2][i] = m
   }
 }
 
