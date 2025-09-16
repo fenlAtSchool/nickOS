744c24d79236766e3bc975a95755bb969d55e5ce
fenlAtSchool <alechan32@nuevaschool.org>
2025-09-16 12:27:27 -0700

    Update and rename buttons.pack to buttons.js

--- experimental/minosBootupLibs/System/Library/buttons.js
+++ experimental/minosBootupLibs/System/Library/buttons.js
@@ -1,8 +1,13 @@
 require('display.pack')
+require('cursors.pack')
 
 class Button extends Window(){
   constructor(x,y,txt, func){
     super(x, y, 5, txt.length * 4)
     this.toExec = func
+    clickFunctions.push(func)
+  }
+  exec(){
+    requestExecFunction(this.toExec, "")
   }
 }
